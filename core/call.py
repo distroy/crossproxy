2472c627756f3c3d15d20543757ecf388ff5d112
distroytan <distroytan@pacewear.cn>
2017-09-28 15:06:12 +0800

    core call: supported for python 2.6.6

--- core/call.py
+++ core/call.py
@@ -6,22 +6,42 @@
 
 
 def call(module_name, function_name, *args, **kargs):
-    import importlib
-
     from core import log
+
+    def import_module(module_name):
+        arr = module_name.split('.')
+        root = arr[0]
+        arr = arr[1:]
+        try:
+            m = __import__(module_name)
+        except Exception as exc:
+            log.error(exc, '__import__(%s) failed', module_name)
+            return
+
+        name = root
+        for i in arr:
+            try:
+                m = getattr(m, i)
+                name = '%s.%s' % (name, i)
+            except Exception as exc:
+                log.error(exc, 'getattr(%s, %s) failed', name, i)
+                return
+        return m
+
 
     if isinstance(module_name, str):
         try:
-            m = importlib.import_module(module_name)
+            m = import_module(module_name)
         except Exception as exc:
             log.error(exc, 'importlib.import_module(%s) failed', module_name)
             return
     else:
         m = module_name
+
     try:
         f = getattr(m, function_name)
     except Exception as exc:
-        log.error(exc, 'getattr(%s, %s) failed', m.__name__, function_name)
+        log.error(exc, 'getattr(%s, %s) failed', module_name, function_name)
         return
 
     return f(*args, **kargs)

f960fb93cba7d0de2a74def0bdec88187eb8b3a5
distroy <001distroy@gmail.com>
2018-11-30 22:23:43 +0800

    script function: fixed check process in darwin

--- script/functions.sh
+++ script/functions.sh
@@ -14,6 +14,16 @@ EXEC_CP0=./$CP0
 EXEC_CP1=./$CP1
 EXEC_CP2=./$CP2
 
+function ps_func() {
+    case $OSTYPE in
+    darwin*)
+        ps -e -ouser -opid -ocommand
+        ;;
+    *)
+        ps f -efj
+    esac
+}
+
 function start_cp0() {
     local listen="$1"
     $EXEC_CP0 -l "$listen" &> /dev/null
@@ -27,7 +37,7 @@ function start_cp1() {
     if [[ "$target" == "" ]]; then
         local target='127.0.0.1:22'
     fi
-    local num=$(ps f -fj -e | grep -w $CP1 | grep "$proxy" | grep -w "$key" | grep -v grep | wc -l)
+    local num=$(ps_func | grep -w $CP1 | grep "$proxy" | grep -w "$key" | grep -v grep | wc -l)
     if (( num == 0 )); then
         # echo $EXEC_CP1 -k "$key" -s "$secret" -p "$proxy" -t "$target"
         $EXEC_CP1 -k "$key" -s "$secret" -p "$proxy" -t "$target"
@@ -39,7 +49,7 @@ function start_cp2() {
     local secret="$2"
     local proxy="$3"
     local listen="$4"
-    local num=$(ps f -fj -e | grep -w $CP2 | grep "$proxy" | grep -w "$key" | grep -v grep | wc -l)
+    local num=$(ps_func | grep -w $CP2 | grep "$proxy" | grep -w "$key" | grep -v grep | wc -l)
     if (( num == 0 )); then
         # echo $EXEC_CP2 -k "$key" -s "$secret" -p "$proxy" -l "$listen"
         $EXEC_CP2 -k "$key" -s "$secret" -p "$proxy" -l "$listen" &> /dev/null
